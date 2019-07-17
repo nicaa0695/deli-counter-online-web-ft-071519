d453b0493118f103052a9469ddf0cd8b715a03e9
Veronica Lopez <nicaa0695@gmail.com>
2019-07-17 20:30:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,5 +3,9 @@ katz_deli = []
 def line(line_array) 
   if line_array.size == 0 
     puts "The line is currently empty."
+  else line_message = "The line is currently: "
+    line_array.each_with_index do |name, index|
+      line_message += "#{index + 1}. #{name}"
   end
+  puts line_message.rstrip
 end
