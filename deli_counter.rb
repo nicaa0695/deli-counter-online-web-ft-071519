0be6744d9705cf90077107d5b4a7e6b037d64ad5
Veronica Lopez <nicaa0695@gmail.com>
2019-07-17 20:32:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,6 +7,6 @@ def line(line_array)
     line_array.each_with_index do |name, index|
       line_message += "#{index + 1}. #{name}"
     end
-    puts line_message
+    puts line_message.rstrip
   end
 end
